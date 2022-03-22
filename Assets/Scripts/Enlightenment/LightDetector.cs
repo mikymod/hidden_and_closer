7a230009487dd5e592e861a41a6dc38366a56d26
Michele Rossi <mikymod@gmail.com>
2022-03-22 01:27:04 +0100

    add light detector prefab

---
+++ Assets/Scripts/Enlightenment/LightDetector.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class LightDetector : MonoBehaviour
+{
+    [SerializeField] private RenderTexture rt;
+    [SerializeField][Range(0f, 0.5f)] private float lightThreshold = 0.05f;
+    private Texture2D texture;
+
+    private bool illuminated = false;
+    public bool Illuminated { get => illuminated; }
+
+    private void Start()
+    {
+        texture = new Texture2D(32, 32, TextureFormat.RGB24, false);
+    }
+
+    private void Update()
+    {
+        RenderTexture.active = rt;
+        texture.ReadPixels(new Rect(0, 0, 32, 32), 0, 0, false);
+        var pixels = texture.GetPixels(0, 0, 32, 32, 0);
+        var avarageLuminosity = AvarageLuminosity(pixels);
+        illuminated = avarageLuminosity > lightThreshold;
+    }
+
+    private float AvarageLuminosity(Color[] colors)
+    {
+        float r = 0f;
+        foreach (var color in colors)
+        {
+            r += color.r;
+        }
+
+        return r / colors.Length;
+    }
+}
