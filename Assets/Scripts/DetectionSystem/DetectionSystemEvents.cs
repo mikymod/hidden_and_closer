563155fee56de0fc8eb210ebd5251abac7442185
Lorenzo Guerrini <lollo.guerrini@gmail.com>
2022-03-23 20:48:49 +0100

    Change Detector for send more action DetectionEnter/Stay/Exit of Audio and Vision

--- Assets/Scripts/DetectionSystem/DetectionSystemEvents.cs
+++ Assets/Scripts/DetectionSystem/DetectionSystemEvents.cs
@@ -3,7 +3,11 @@ using UnityEngine.Events;
 
 namespace HNC {
     public class DetectionSystemEvents : MonoBehaviour {
-        public static UnityAction<GameObject, GameObject> OnVisionDetect;
-        public static UnityAction<GameObject, GameObject> OnAudioDetect;
+        public static UnityAction<GameObject, GameObject> OnVisionDetectEnter;
+        public static UnityAction<GameObject, GameObject> OnVisionDetectStay;
+        public static UnityAction<GameObject, GameObject> OnVisionDetectExit;
+        public static UnityAction<GameObject, GameObject> OnAudioDetectEnter;
+        public static UnityAction<GameObject, GameObject> OnAudioDetectStay;
+        public static UnityAction<GameObject, GameObject> OnAudioDetectExit;
     }
 }
--- Assets/Scripts/DetectionSystem/Detector.cs
+++ Assets/Scripts/DetectionSystem/Detector.cs
@@ -1,6 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+internal enum DetectedState {
+    None,
+    Releved,
+    FirstNotified,
+    LastNotified,
+}
+
 namespace HNC {
     [RequireComponent(typeof(SphereCollider))]
     public class Detector : MonoBehaviour {
@@ -11,29 +18,43 @@ namespace HNC {
 
         private float _radius;
         private SphereCollider _collider;
+
         private Transform _playerTransform;
-        private List<Transform> _audioEmitter;
-        private List<Transform> _senderAudioEmitter;
+        private DetectedState _playerState = DetectedState.None;
+
+        private List<Transform> _soundEmittersTransform;
+        private Dictionary<Transform, DetectedState> _soundEmittersState;
+
         private Ray _ray;
         private RaycastHit _hit;
         private Vector3 _distance;
 
         private void Awake() {
-            _audioEmitter = new List<Transform>();
-            _senderAudioEmitter = new List<Transform>();
+            _soundEmittersTransform = new List<Transform>();
+            _soundEmittersState = new Dictionary<Transform, DetectedState>();
+
             _collider = GetComponent<SphereCollider>();
             _collider.isTrigger = true;
             _radius = _collider.radius;
         }
 
-        private void OnEnable() => AudioManager.OnDoingNoise += CheckAudio;
+        private void OnEnable() {
+            AudioManager.OnSoundPlay += AddSoundEmitter;
+            AudioManager.OnSoundStop += RemoveSoundEmitter;
 
-        private void OnDisable() => AudioManager.OnDoingNoise -= CheckAudio;
+        }
+
+
+        private void OnDisable() {
+            AudioManager.OnSoundPlay -= AddSoundEmitter;
+            AudioManager.OnSoundStop -= RemoveSoundEmitter;
+        }
 
         private void OnTriggerEnter(Collider other) {
             //Save reference to Player Transform
-            if (other.gameObject.layer == LayerMask.NameToLayer(PlayerLayerName)) {
+            if (other.gameObject.layer == LayerMask.NameToLayer(PlayerLayerName) && _playerTransform == null) {
                 _playerTransform = other.transform;
+                _playerState = DetectedState.Releved;
             }
         }
 
@@ -46,30 +67,103 @@ namespace HNC {
                     Debug.DrawLine(_ray.origin, _playerTransform.position, Color.green);
                     if (Physics.Raycast(_ray, out _hit, _radius, VisionLayerMask)) {
                         if (_hit.collider.transform == _playerTransform) {
-                            DetectionSystemEvents.OnVisionDetect.Invoke(gameObject, _playerTransform.gameObject);
+                            switch (_playerState) {
+                                case DetectedState.None:
+                                    Debug.LogWarning("Error in detection System, not releved player was shoot by raycast", gameObject);
+                                    break;
+                                case DetectedState.Releved:
+                                    DetectionSystemEvents.OnVisionDetectEnter?.Invoke(gameObject, _playerTransform.gameObject);
+                                    _playerState = DetectedState.FirstNotified;
+                                    break;
+                                case DetectedState.FirstNotified:
+                                    DetectionSystemEvents.OnVisionDetectStay?.Invoke(gameObject, _playerTransform.gameObject);
+                                    break;
+                                case DetectedState.LastNotified:
+                                    DetectionSystemEvents.OnVisionDetectEnter?.Invoke(gameObject, _playerTransform.gameObject);
+                                    _playerState = DetectedState.FirstNotified;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        } else {
+                            if (_playerState == DetectedState.FirstNotified) {
+                                DetectionSystemEvents.OnVisionDetectExit?.Invoke(gameObject, _playerTransform.gameObject);
+                                _playerState = DetectedState.LastNotified;
+                            }
                         }
+                    } else {
+                        if (_playerState == DetectedState.FirstNotified) {
+                            DetectionSystemEvents.OnVisionDetectExit?.Invoke(gameObject, _playerTransform.gameObject);
+                            _playerState = DetectedState.LastNotified;
+                        }
+                    }
+                } else {
+                    if (_playerState == DetectedState.FirstNotified) {
+                        DetectionSystemEvents.OnVisionDetectExit?.Invoke(gameObject, _playerTransform.gameObject);
+                        _playerState = DetectedState.LastNotified;
                     }
                 }
             }
             //Audio detected
-            for (int i = _audioEmitter.Count -1 ; i >= 0; i--) {
+            for (int i = 0; i < _soundEmittersTransform.Count; i++) {
                 //Check distance
-                _distance = _audioEmitter[i].position - transform.position;
+                _distance = _soundEmittersTransform[i].position - transform.position;
                 if (_distance.sqrMagnitude < _radius * _radius) {
-                    Debug.Log("Send Audio even", _audioEmitter[i].gameObject);
-                    DetectionSystemEvents.OnAudioDetect.Invoke(gameObject, _audioEmitter[i].gameObject);
+                    switch (_soundEmittersState[_soundEmittersTransform[i]]) {
+                        case DetectedState.None:
+                            Debug.LogWarning("Error in detection System, sound NONE was releved", gameObject);
+                            break;
+                        case DetectedState.Releved:
+                            DetectionSystemEvents.OnAudioDetectEnter?.Invoke(gameObject, _soundEmittersTransform[i].gameObject);
+                            _soundEmittersState[_soundEmittersTransform[i]] = DetectedState.FirstNotified;
+                            break;
+                        case DetectedState.FirstNotified:
+                            DetectionSystemEvents.OnAudioDetectStay?.Invoke(gameObject, _soundEmittersTransform[i].gameObject);
+                            break;
+                        case DetectedState.LastNotified:
+                            DetectionSystemEvents.OnAudioDetectEnter?.Invoke(gameObject, _soundEmittersTransform[i].gameObject);
+                            _soundEmittersState[_soundEmittersTransform[i]] = DetectedState.FirstNotified;
+                            break;
+                        default:
+                            break;
+                    }
+                } else {
+                    if (_soundEmittersState[_soundEmittersTransform[i]] == DetectedState.FirstNotified) {
+                        DetectionSystemEvents.OnAudioDetectExit?.Invoke(gameObject, _soundEmittersTransform[i].gameObject);
+                        _soundEmittersState[_soundEmittersTransform[i]] = DetectedState.LastNotified;
+                    }
                 }
             }
         }
 
-        private void OnTriggerExit(Collider other) => _playerTransform = null;
+        private void OnTriggerExit(Collider other) {
+            if (_playerTransform != null && other.transform == _playerTransform) {
+                if (_playerState == DetectedState.FirstNotified) {
+                    DetectionSystemEvents.OnVisionDetectExit?.Invoke(gameObject, _playerTransform.gameObject);
+                }
+                _playerTransform = null;
+                _playerState = DetectedState.None;
+            }
+        }
 
-        private void CheckAudio(Transform emitter) {
-            Debug.Log("Recive audio event");
-            if (_audioEmitter.Contains(emitter)) {
+        private void AddSoundEmitter(AudioClipsBankSO uslessACB, AudioConfigurationSO uslessAC, Transform emitter, float uslessF) {
+            if (_soundEmittersTransform.Contains(emitter)) {
                 return;
             }
-            _audioEmitter.Add(emitter);
+            _soundEmittersTransform.Add(emitter);
+            _soundEmittersState.Add(emitter, DetectedState.Releved);
+        }
+
+        private void RemoveSoundEmitter(Transform emitter, float usless) {
+            if (!_soundEmittersTransform.Contains(emitter)) {
+                return;
+            }
+            if (_soundEmittersState[emitter] == DetectedState.FirstNotified) {
+                DetectionSystemEvents.OnAudioDetectExit?.Invoke(gameObject, emitter.gameObject);
+                _soundEmittersState[emitter] = DetectedState.LastNotified;
+            }
+            _soundEmittersTransform.Remove(emitter);
+            _soundEmittersState.Remove(emitter);
         }
     }
 }
