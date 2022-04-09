43fdd84564b5530ac8d58054e8f71fda72f53497
Michele Rossi <m.rossi@overside.it>
2022-04-09 18:57:30 +0200

    fix player spawner, cleanup

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -1,26 +1,30 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace HNC
 {
     public class PlayerSpawner : MonoBehaviour
     {
         [SerializeField] private GameObject playerPrefab;
-        [SerializeField] private Transform defaultSpawnPoint;
         [SerializeField] private SaveSystem saveSystem;
 
         private void Start()
         {
             if (saveSystem.SaveData.Player.Scene != null && saveSystem.SaveData.Player.Scene != "")
             {
-                Instantiate(playerPrefab, saveSystem.SaveData.Player.Position, Quaternion.identity);
+                Spawn(saveSystem.SaveData.Player.Position, Quaternion.identity);
             }
             else
             {
-                Instantiate(playerPrefab, defaultSpawnPoint.position, defaultSpawnPoint.rotation);
+                Spawn(transform.position, transform.rotation);
             }
+        }
+
+        private void Spawn(Vector3 position, Quaternion rotation)
+        {
+            Instantiate(playerPrefab, position, rotation);
+            SaveSystem.PlayerSave?.Invoke(SceneManager.GetActiveScene(), transform, true); // FIXME: manage companion
+            SaveSystem.LevelStarted?.Invoke(SceneManager.GetActiveScene(), transform);
         }
     }
 }
