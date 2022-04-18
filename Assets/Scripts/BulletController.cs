4471d7fc18d764dfe56a2b5c532a0b036f8e2aff
Michele Rossi <m.rossi@overside.it>
2022-04-18 09:49:29 +0200

    add particles to bullet

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,20 +4,51 @@ using UnityEngine;
 
 namespace HNC
 {
-    // FIXME: temp behaviour
     public class BulletController : MonoBehaviour
     {
         private Rigidbody _rb;
         private bool _hasCollide = false;
 
-        private void Awake() => _rb = GetComponent<Rigidbody>();
+        [SerializeField] private GameObject bloodParticlesPrefab;
+        [SerializeField] private GameObject dustParticlesPrefab;
+        private GameObject _bloodGO;
+        private ParticleSystem _bloodParticles;
+        private GameObject _dustGO;
+        private ParticleSystem _dustParticles;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _bloodGO = Instantiate(bloodParticlesPrefab, Vector3.zero, Quaternion.identity);
+            _bloodParticles = _bloodGO.GetComponent<ParticleSystem>();
+            _bloodParticles.Stop();
+            _dustGO = Instantiate(dustParticlesPrefab, Vector3.zero, Quaternion.identity);
+            _dustParticles = _dustGO.GetComponent<ParticleSystem>();
+            _dustParticles.Stop();
+        }
 
         private void OnCollisionEnter(Collision other)
         {
-            if (!_hasCollide && other.gameObject.TryGetComponent(out NewEnemyController enemy))
+            if (_hasCollide)
+            {
+                return;
+            }
+
+            if (other.gameObject.TryGetComponent(out NewEnemyController enemy))
             {
                 enemy.Damaged();
+
+                _bloodGO.transform.position = other.GetContact(0).point;
+                _bloodGO.transform.rotation = Quaternion.LookRotation(other.GetContact(0).normal, Vector3.up);
+                _bloodParticles.Play();
             }
+            else
+            {
+                _dustGO.transform.position = other.GetContact(0).point;
+                _dustGO.transform.rotation = Quaternion.LookRotation(other.GetContact(0).normal, Vector3.up);
+                _dustParticles.Play();
+            }
+
             _hasCollide = true;
             StartCoroutine(Reset());
             transform.GetComponent<AudioRockController>().PlayHitSound();
@@ -30,6 +61,17 @@ namespace HNC
             _hasCollide = false;
             _rb.useGravity = false;
             _rb.isKinematic = true;
+
+            if (_dustParticles.isPlaying)
+            {
+                _dustParticles.Stop();
+            }
+
+            if (_bloodParticles.isPlaying)
+            {
+                _bloodParticles.Stop();
+            }
+
             gameObject.SetActive(false);
         }
     }
